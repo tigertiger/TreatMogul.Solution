12934b63f6f21d38c3903a4738074355f31480c4
Melissa <melissa.scmi@gmail.com>
2021-10-22 14:51:50 -0700

    create dynamic model to display splash w treats & flavs

--- TreatMogul/Controllers/HomeController.cs
+++ TreatMogul/Controllers/HomeController.cs
@@ -1,12 +1,34 @@
 using Microsoft.AspNetCore.Mvc;
+using TreatMogul.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
 
 namespace TreatMogul.Controllers
 {
   public class HomeController : Controller
   {
+    private readonly TreatMogulContext _db;
+
+    public HomeController(TreatMogulContext db)
+    {
+      _db = db;
+    }
+
     [HttpGet("/")]
     public ActionResult Index() { 
       return View(); 
+    }
+  
+    [HttpGet("Menu")]
+    public ActionResult Menu() 
+    {
+      dynamic model = new ExpandoObject();
+      model.Treat = _db.Treats.ToList();
+      model.Flavor = _db.Flavors.ToList();
+      return View(model); 
     }
   }
 }
